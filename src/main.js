7c5842df003b593dd3be42305137d0bed40bcc4e
Jahmanz Williams <savojah@yahoo.com>
2018-03-26 23:29:23 -0700

    adds api key, css styling

--- src/main.js
+++ src/main.js
@@ -10,28 +10,29 @@ $(document).ready(function() {
 
  let docsearch = $("#docsearch").val();
 
-$.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docsearch}&location=45.522,-122.682,100&skip=2&limit=10&user_key=772567d7e5113ceaeea1ac0b1cd56630`).then(function(response){
+$.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docsearch}&location=45.522,-122.682,100&skip=2&limit=10&user_key=` + process.env.API_KEY).then(function(response){
       console.log(response);
 
   if(response.data.length === 0)
       {
     $("#results").text("No Doctors match that search. Please try again!");
       }
+
   for(let i = 0; i < response.data.length; i++)
       {
-        $("#results").append(`<h3>${response.data[i].practices[0].name} </h3>
+        $("#results").append(`<div class = "border"><h3>${response.data[i].practices[0].name} </h3>
           <br>
           <h2>Specialty: ${response.data[i].specialties[0].name} </h2>
-          <br><tr>
-          <h2> ${response.data[i].practices[0].visit_address.street},<h2>
-          <h2>${response.data[i].practices[0].visit_address.city},</h2>
-          <h2>${response.data[i].practices[0].visit_address.state}</h2>
-          <h2>${response.data[i].practices[0].visit_address.zip} </h2>
-          </tr>
+          <br>
+          <h2> ${response.data[i].practices[0].visit_address.street},
+          ${response.data[i].practices[0].visit_address.city},
+          ${response.data[i].practices[0].visit_address.state}
+          ${response.data[i].practices[0].visit_address.zip} </h2>
           <br>
         <h2> Phone: ${response.data[i].practices[0].phones[0].number} </h2>
         <br>
-        <h2> Accepting Patients:  ${response.data[i].practices[0].accepts_new_patients} </h2>`);
+        <h2> Accepting Patients:  ${response.data[i].practices[0].accepts_new_patients} </h2>
+        <br></div>`);
     }
 
 
@@ -49,7 +50,7 @@ $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docsearch}&locatio
 
   let specialtysearch = $("#specialtysearch").val();
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${specialtysearch}&location=45.522,-122.682,100&skip=2&limit=10&user_key=772567d7e5113ceaeea1ac0b1cd56630`).then(function(response){
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${specialtysearch}&location=45.522,-122.682,100&skip=2&limit=10&user_key=` + process.env.API_KEY).then(function(response){
       console.log(response);
 
   if(response.data.length === 0)
@@ -58,19 +59,18 @@ $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docsearch}&locatio
       }
   for(let i = 0; i < response.data.length; i++)
       {
-        $("#results").append(`<h3>${response.data[i].practices[0].name} </h3>
+        $("#results").append(`<div class = "border"><h3>${response.data[i].practices[0].name} </h3>
           <br>
           <h2>Specialty: ${response.data[i].specialties[0].name} </h2>
-          <br><tr>
-          <h2> ${response.data[i].practices[0].visit_address.street},<h2>
-          <h2>${response.data[i].practices[0].visit_address.city},</h2>
-          <h2>${response.data[i].practices[0].visit_address.state}</h2>
-          <h2>${response.data[i].practices[0].visit_address.zip} </h2>
-          </tr>
+          <br>
+          <h2> ${response.data[i].practices[0].visit_address.street},
+          ${response.data[i].practices[0].visit_address.city},
+          ${response.data[i].practices[0].visit_address.state}
+          ${response.data[i].practices[0].visit_address.zip} </h2>
           <br>
         <h2> Phone: ${response.data[i].practices[0].phones[0].number} </h2>
         <br>
-        <h2> Accepting Patients:  ${response.data[i].practices[0].accepts_new_patients} </h2>`);
+        <h2> Accepting Patients:  ${response.data[i].practices[0].accepts_new_patients} </h2><br></div>`);
     }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -14,13 +15,15 @@ module.exports = {
       contentBase: './dist'
     },
   plugins: [
-     new UglifyJsPlugin({ sourceMap: true }),
+    new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'docsearch',
       template: './src/index.html',
       inject: 'body'
-    })
+
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
